4b2f65ab6b9a72063f78cd873303dbdde55fe895
William Abreu <wabreu@fullstacklabs.co>
2023-03-15 14:12:42 -0300

    add project service, add service to dependency injection config

--- aspnet-core/Klir.TechChallenge.Infrastructure/Configurator/ServiceConfigurator.cs
+++ aspnet-core/Klir.TechChallenge.Infrastructure/Configurator/ServiceConfigurator.cs
@@ -1,5 +1,6 @@
 using Klir.TechChallenge.Domain.Interfaces;
 using Klir.TechChallenge.Repository.Repositories;
+using Klir.TechChallenge.Service.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Klir.TechChallenge.Infrastructure.Configurator
@@ -8,6 +9,9 @@ namespace Klir.TechChallenge.Infrastructure.Configurator
     {
         public static void ConfigureServices(IServiceCollection services)
         {
+            //Services
+            services.AddTransient<IProductService, ProductService>();
+
             //Repositories
             services.AddTransient<IProductRepository, ProductRepository>();
         }
---
+++ aspnet-core/Klir.TechChallenge.Service/Services/ProductService.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Klir.TechChallenge.Domain.Interfaces;
+using Klir.TechChallenge.Domain.Models;
+
+namespace Klir.TechChallenge.Service.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public int Add(Product product)
+        {
+            return _productRepository.Add(product);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _productRepository.GetAll();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productRepository.GetById(id);
+        }
+    }
+}
